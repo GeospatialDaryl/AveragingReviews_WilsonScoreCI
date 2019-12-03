11b6bac67bae6ed446f7d9cab7ee8d6b042acdba
Daryl Van Dyke <GeospatialDaryl@users.noreply.github.com>
2019-12-03 10:35:43 -0800

    sql

---
+++ ci_lowerbound.sql
@@ -0,0 +1,5 @@
+SELECT widget_id, ((positive + 1.9208) / (positive + negative) - 
+                   1.96 * SQRT((positive * negative) / (positive + negative) + 0.9604) / 
+                          (positive + negative)) / (1 + 3.8416 / (positive + negative)) 
+       AS ci_lower_bound FROM widgets WHERE positive + negative > 0 
+       ORDER BY ci_lower_bound DESC;
